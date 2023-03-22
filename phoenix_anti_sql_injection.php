1839bae0995b20289d2afe056f53abc6aaa2901b
Wellington-Albuquerque <85085734+wellington043@users.noreply.github.com>
2023-03-21 21:24:30 -0300

    Update phoenix_anti_sql_injection.php

--- phoenix_anti_sql_injection.php
+++ phoenix_anti_sql_injection.php
@@ -2,58 +2,44 @@
 
 /* 
 	|--------------------------------------------------------------------------------|
-
-	ANTI SQL INJECTION BY: Gabriel Aparecido - (Phoenix) 
+	ANTI SQL INJECTION V1 BY: Gabriel Aparecido - (Phoenix) 
 	GITHUB: https://github.com/Phoenix-code21
-	|---------------------------------------------------------------------------------|
-
+	|--------------------------------------------------------------------------------|
+	I edit New Version (V2) 
+		--update preg_replace to accept email/url/name/numbers
+		--upgraded to a single function, better performance and more readable
+		
+	
 	Como usar?
 	R: abra o index.php ou arquivo principal do seu site/sistema e inclua esse script na primeira linha
 	de código.
 
-	require_once 'phoenix_anti_sql_injection.php'
-
-*/
-
-/* 
 	Você desabilitar a função ANTI INJECTION inserindo o nome do campo dentro
 	da matriz allowFieldPOST ou allowFieldGET
 */
 
-$allowFieldPOST = array(
-	'texto',
-	'titulo',
-	'senha',
-);
 
-$allowFieldGET = array(
-	'url'
-);
+$allowFieldPOST = [
+  'array-names-exemple',
+];
 
-if(isset($_POST)){
-	foreach($_POST as $key => $value){
-		if(!in_array($key,$allowFieldPOST)){
-			if(!is_numeric($_POST[$key])){
-				$_POST[$key] =  preg_replace('/[^[:alpha:]_]/', '',$value);
-			}else{
-				$_POST[$key] = $value;
-			}
-		}else{
-			$_POST[$key] = $value;
-		}
-	}
+$allowFieldGET = [];
+
+function cleanInputData($data, $allowedFields)
+{
+  $cleanedData = [];
+
+  foreach ($data as $key => $value) {
+    if (!in_array($key, $allowedFields)) {
+      $cleanedData[$key] = (is_numeric($value)) ? $value : preg_replace('/[^a-z A-Z0-9\/:.,_@-]/', '', $value);
+    } else {
+      $cleanedData[$key] = $value;
+    }
+  }
+
+  return $cleanedData;
 }
 
-if(isset($_GET)){
-	foreach($_GET as $key => $value){
-		if(!in_array($key,$allowFieldGET)){
-			if(!is_numeric($_GET[$key])){
-				$_GET[$key] = preg_replace('/[^[:alpha:]_]/', '',$value);
-			}else{
-				$_GET[$key] = $value;
-			}
-		}else{
-			$_GET[$key] = $value;
-		}
-	}
-}
+$_POST = cleanInputData($_POST, $allowFieldPOST);
+$_GET = cleanInputData($_GET, $allowFieldGET);
+
